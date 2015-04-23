8d549c6e51b96215a078bc407f043861ad894d94
Tobias Lidskog <tobias@lidskog.se>
2015-04-23 11:18:37 +0200

    Rename call to _call to indicate that it's private
    This has the added benefit of excluding it by default from Bluebird's promisification, see https://github.com/petkaantonov/bluebird/blob/master/API.md#option-filter.

--- index.js
+++ index.js
@@ -17,93 +17,93 @@ var MobProxy = function(cfg) {
 
 MobProxy.prototype = {
     getProxyList: function(callback) {
-        this.call('GET', '/proxy', null, callback);
+        this._call('GET', '/proxy', null, callback);
     },
 
     startPort: function(port, callback) {
-        this.call('POST', '/proxy', 'port=' + port, callback);
+        this._call('POST', '/proxy', 'port=' + port, callback);
     },
 
     stopPort: function(port, callback) {
-        this.call('DELETE', '/proxy/' + port, null, callback);
+        this._call('DELETE', '/proxy/' + port, null, callback);
     },
 
     createHAR: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/har', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/har', parameterize(cfg), callback);
     },
 
     startNewPage: function(port, pageRef, callback) {
-        this.call('PUT', '/proxy/' + port + '/har/pageRef', pageRef ? pageRef : '', callback);
+        this._call('PUT', '/proxy/' + port + '/har/pageRef', pageRef ? pageRef : '', callback);
     },
 
     getHAR: function(port, callback) {
-        this.call('GET', '/proxy/' + port + '/har', null, callback);
+        this._call('GET', '/proxy/' + port + '/har', null, callback);
     },
 
     limit: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/limit', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/limit', parameterize(cfg), callback);
     },
 
     addURLWhiteList: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/whitelist', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/whitelist', parameterize(cfg), callback);
     },
 
     clearURLWhiteList: function(port, callback) {
-        this.call('DELETE', '/proxy/' + port + '/whitelist', null, callback);
+        this._call('DELETE', '/proxy/' + port + '/whitelist', null, callback);
     },
 
     addURLBlackList: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/blacklist', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/blacklist', parameterize(cfg), callback);
     },
 
     clearURLBlackList: function(port, callback) {
-        this.call('DELETE', '/proxy/' + port + '/blacklist', null, callback);
+        this._call('DELETE', '/proxy/' + port + '/blacklist', null, callback);
     },
 
     setHeaders: function(port, json, callback) {
-        this.call('POST', '/proxy/' + port + '/headers', json, callback, true);
+        this._call('POST', '/proxy/' + port + '/headers', json, callback, true);
     },
 
     setDNSLookupOverride: function(port, json, callback) {
-        this.call('POST', '/proxy/' + port + '/hosts', json, callback, true);
+        this._call('POST', '/proxy/' + port + '/hosts', json, callback, true);
     },
 
     setAuthentication: function(port, domain, json, callback) {
-        this.call('POST', '/proxy/' + port + '/auth/basic/' + domain, json, callback, true);
+        this._call('POST', '/proxy/' + port + '/auth/basic/' + domain, json, callback, true);
     },
 
     setWaitPeriod: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/wait', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/wait', parameterize(cfg), callback);
     },
 
     setTimeouts: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/timeout', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/timeout', parameterize(cfg), callback);
     },
 
     addURLRedirect: function(port, cfg, callback) {
-        this.call('PUT', '/proxy/' + port + '/rewrite', parameterize(cfg), callback);
+        this._call('PUT', '/proxy/' + port + '/rewrite', parameterize(cfg), callback);
     },
 
     removeAllURLRedirects: function(port, callback) {
-        this.call('DELETE', '/proxy/' + port + '/rewrite', null, callback);
+        this._call('DELETE', '/proxy/' + port + '/rewrite', null, callback);
     },
 
     setRetryCount: function(port, retryCount, callback) {
-        this.call('PUT', '/proxy/' + port + '/retry', 'retryCount=' + retryCount, callback);
+        this._call('PUT', '/proxy/' + port + '/retry', 'retryCount=' + retryCount, callback);
     },
 
     clearDNSCache: function(port, callback) {
-        this.call('DELETE', '/proxy/' + port + '/dns/cache', null, callback);
+        this._call('DELETE', '/proxy/' + port + '/dns/cache', null, callback);
     },
 
     addRequestInterceptor: function(port, payload, callback) {
-        this.call('POST', '/proxy/' + port + '/interceptor/request', payload, callback);
+        this._call('POST', '/proxy/' + port + '/interceptor/request', payload, callback);
     },
 
     addResponseInterceptor: function(port, payload, callback) {
-        this.call('POST', '/proxy/' + port + '/interceptor/response', payload, callback);
+        this._call('POST', '/proxy/' + port + '/interceptor/response', payload, callback);
     },
-    call: function(method, url, data, callback, isJson) {
+    _call: function(method, url, data, callback, isJson) {
         var self = this;
         var contentType = isJson ? 'application/json' : 'application/x-www-form-urlencoded';
         var options = {
